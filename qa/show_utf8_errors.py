cac8eb70692735e37a1e7c3999a099240c6238d7
Jacob Fenton <jsfenfen@gmail.com>
2017-10-18 15:12:47 -0700

    redo concordance with csvformat -U 0

--- qa/show_utf8_errors.py
+++ qa/show_utf8_errors.py
@@ -1,21 +1,23 @@
 INFILE = '../efiler_master_concordance.csv'
 
 def test_utf8(data):
-    ""
     try:
         data.decode('utf-8')
         return False
     except UnicodeDecodeError:
         return True
 
-if __name__ == '__main__':
-    filein = open(INFILE, 'rb')
+def test_file(filename):
+    filein = open(filename, 'rb')
     error_count = 0
     for i, line in enumerate(filein):
-
         result = test_utf8(line)
         if result == 1:
-            print("\nNot UTF-8 at line %s: '%s'" % (i, line))
+            print("\nNot UTF-8 at line %s: '%s'" % (i+1, line))  # 'enumerate' is 0-indexed
             error_count += 1
-      
-    print("Total errors: %s" % error_count)
+    print("Total of %s errors in file '%s'" % (error_count, filename))
+
+
+if __name__ == '__main__':
+    test_file(INFILE)
+
